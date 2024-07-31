4a98ff405cff61753b358afac2ebf88b797e87c2
ckdgns6078 <101166430+ckdgns6078@users.noreply.github.com>
2024-07-31 14:35:54 +0900

    solved Boj1600.java

--- hoon/BFS/Boj1600.java
+++ hoon/BFS/Boj1600.java
@@ -5,19 +5,21 @@ import java.util.Queue;
 import java.util.Scanner;
 
 class Point2 {
-	int y;
-	int x;
+	int r;
 	int c;
+	int cnt;
+	int horse;
 
-	Point2(int y, int x, int c) {
-		this.y = y;
-		this.x = x;
+	Point2(int r, int c, int cnt, int horse) {
+		this.r = r;
 		this.c = c;
+		this.cnt = cnt;
+		this.horse = horse;
 	}
 
 }
 
-public class Boj1600 {
+public class Main {
 	static int K, H, W;
 	static int[][] map;
 	static boolean[][][] visited;
@@ -26,81 +28,53 @@ public class Boj1600 {
 		Scanner sc = new Scanner(System.in);
 
 		K = sc.nextInt();
-		H = sc.nextInt();
 		W = sc.nextInt();
-		map = new int[W][H];
-		visited = new boolean[2][W][H];
-		for (int i = 0; i < W; i++) {
-			for (int j = 0; j < H; j++) {
+		H = sc.nextInt();
+		map = new int[H][W];
+
+		visited = new boolean[H][W][K + 1];
+//		visited = new boolean[K][H][W];
+		for (int i = 0; i < H; i++) {
+			for (int j = 0; j < W; j++) {
 				map[i][j] = sc.nextInt();
 
 			}
 		}
 
-		System.out.println(bfs());
-
-		for (int i = 0; i < W; i++) {
-			System.out.println(Arrays.toString(map[i]));
-		}
+		bfs();
 
 	}
 
-	private static int bfs() {
-		int[] xarr = { -1, 1, 0, 0 };
-		int[] yarr = { 0, 0, 1, -1 };
-		int[] kx = { 1, 2, 2, 1, -1, -2, -2, -1 };
-		int[] ky = { -2, -1, 1, 2, 2, 1, -1, -2 };
+	private static void bfs() {
+
+		int[] dr = { -1, 1, 0, 0, 1, 2, 2, 1, -1, -2, -2, -1 };
+		int[] dc = { 0, 0, 1, -1, -2, -1, 1, 2, 2, 1, -1, -2 };
 
 		Queue<Point2> q = new ArrayDeque();
-		q.offer(new Point2(0, 0, 0));
+		q.offer(new Point2(0, 0, 0, 0));
 		visited[0][0][0] = true;
-		visited[1][0][0] = true;
-
-		int result = 0;
-		int temp = 2;
+		boolean check = false;
 		// 갈 수 있으면 이동하고 이동하지 못하면 칸이동
 		while (!(q.isEmpty())) {
-			int size = q.size();
-			for (int k = 0; k < size; k++) {
-				Point2 p = q.poll();
-				System.out.println("px: " +p.x);
-				System.out.println("py: " +p.y);
-				System.out.println("pc: " +p.c);
-				for (int i = 0; i < W; i++) {
-					System.out.println(Arrays.toString(map[i]));
-				}
-				System.out.println("===================");
-				int x;
-				int y;
-				if (p.c < K) {
-					for (int i = 0; i < kx.length; i++) {
-						x = p.x + kx[i];
-						y = p.y + ky[i];
-						if (x >= 0 && x < H && y >= 0 && y < W && !visited[0][y][x] && map[y][x] != 1) {
-							map[y][x] = temp;
-							visited[0][y][x] = true;
-							q.offer(new Point2(y, x, p.c + 1));
-						}
-					}
-				}
-				for (int i = 0; i < xarr.length; i++) {
-					x = p.x + xarr[i];
-					y = p.y + yarr[i];
-					if (x >= 0 && x < H && y >= 0 && y < W && !visited[1][y][x] && map[y][x] != 1) {
-						map[y][x] = temp;
-						visited[1][y][x] = true;
-						q.offer(new Point2(y, x, p.c));
-					}
+			Point2 p = q.poll();
+			if (p.r == H - 1 && p.c == W - 1) {
+				System.out.println(p.cnt);
+				check = true;
+				break;
+			}
+			for (int d = 0; p.horse >= K ? d < 4 : d < 12; d++) {
+				int nr = p.r + dr[d];
+				int nc = p.c + dc[d];
+				int nHorse = d < 4 ? p.horse : p.horse + 1;
+				if (nr >= 0 && nr < H && nc >= 0 && nc < W && !visited[nr][nc][nHorse] && map[nr][nc] != 1) {
+					visited[nr][nc][nHorse] = true;
+					q.offer(new Point2(nr, nc, p.cnt + 1, nHorse));
 				}
 			}
-
-			temp++;
-		}
-		if (map[W - 1][H - 1] == 0) {
-			return -1;
 		}
 
-		return (map[W - 1][H - 1] - 1);
+		if (!check)
+			System.out.println(-1);
+
 	}
-
 }
