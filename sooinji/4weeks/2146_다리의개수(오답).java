e5652ce0f5cb1af9e68e6dec3bd937fef97339d8
수인 <121311428+sooinji@users.noreply.github.com>
2024-08-14 00:40:01 +0900

    Update 2146_다리의개수(오답).java

--- sooinji/4weeks/2146_다리의개수(오답).java
+++ sooinji/4weeks/2146_다리의개수(오답).java
@@ -14,9 +14,8 @@ class Point {
 }
 public class Main {
 	
-	static int n, cnt = 0;
+	static int n, cnt = 1;
 	static int[][] map;
-	static boolean[][] vis;
 	static int[] dx = {-1, 1, 0, 0};
 	static int[] dy = {0, 0, -1, 1};
 	static int ans = Integer.MAX_VALUE;
@@ -24,7 +23,6 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		n = sc.nextInt();
 		map = new int[n][n];
-		vis = new boolean[n][n];
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n; j++) {
 				map[i][j] = sc.nextInt();
@@ -32,7 +30,7 @@ public class Main {
 		}
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n; j++) {
-				if (!vis[i][j] && map[i][j] == 1) {
+				if (map[i][j] == 1) {
 					cnt++;
 					bfs(i, j);
 				}
@@ -41,22 +39,14 @@ public class Main {
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n; j++) {
 				if (map[i][j] > 1) {
-					for (int d = 0; d < dx.length; d++) {
-						int nx = i + dx[d];
-						int ny = j + dy[d];
-						if (nx < 0 || nx >= n || ny < 0 || ny >=n) continue;
-						if (map[nx][ny] == 0) { // 바다와 인접
-							bfs2(i, j);
-							break;
-						}
-					}
+					bfs2(i, j);
 				}
 			}
 		}
 		System.out.println(ans - 1);
 	}
 	private static void bfs2(int x, int y) {
-		vis = new boolean[n][n];
+		boolean[][] vis = new boolean[n][n];
 		Queue<Point> q = new ArrayDeque<>();
 		q.offer(new Point(x, y, 0));
 		vis[x][y] = true;
@@ -70,11 +60,15 @@ public class Main {
 				if (nx < 0 || nx >= n || ny < 0 || ny >=n) continue;
 				// 방문 o 점
 				if (vis[nx][ny]) continue;
+				// 육지일 때 
+				if (map[nx][ny] == map[x][y]) continue;
+				// 다른 섬에 도착했으면 탐색 종료
+				if (map[nx][ny] != 0 && map[x][y] != map[nx][ny]) {
+					if (nd < ans) ans = nd;
+					break;
+				}
 				q.offer(new Point(nx, ny, nd));
 				vis[nx][ny] = true;
-				if (map[x][y] != 0 && map[nx][ny] != 0 && map[x][y] != map[nx][ny]) {
-					if (nd < ans) ans = nd;
-				}
 			}
 		}
 	}
@@ -83,7 +77,6 @@ public class Main {
 		q.offer(new Point(x, y));
 		while(!q.isEmpty()) {
 			Point cur = q.poll();
-			vis[cur.x][cur.y] = true;
 			map[cur.x][cur.y] = cnt;
 			for (int i = 0; i < dx.length; i++) {
 				int nx = cur.x + dx[i];
@@ -91,7 +84,7 @@ public class Main {
 				// 범위 밖
 				if (nx < 0 || nx >= n || ny < 0 || ny >=n) continue;
 				// 방문 o 점, 바다
-				if (vis[nx][ny] || map[nx][ny] == 0) continue;
+				if (map[nx][ny] > 1 || map[nx][ny] == 0) continue;
 				q.offer(new Point(nx, ny));
 			}
 		}
